e09a8f8267e6127bcba4e9c0ef175e4e871b427a
Alexis <awo@opinum.com>
2018-02-16 09:42:33 +0100

    Words Animation + body fadout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,23 @@
 import '../sass/app.scss';
 
+function sleep (time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+}
+
+
+function bodyFade() {
+    let b = document.querySelector('body')
+
+    b.style.opacity = '0' ? b.style.opacity = '1' : b.style.opacity = '0'
+};
+
+sleep(1000).then(() => {
+    bodyFade()
+});
+
+
+
+
 (function stringSplit() {
     let w = document.querySelectorAll('.letter__wrapper')
 
@@ -21,11 +39,24 @@ function lettersAnimate() {
     }
 };
 
-
-function sleep (time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-
-sleep(1000).then(() => {
+sleep(4000).then(() => {
     lettersAnimate()
 });
+
+
+
+
+
+
+
+(function wordsAnimate() {
+    let w = document.querySelectorAll('.word')
+
+    for (let i = 0; i < w.length; i++) {
+        const el = w[i]
+
+        sleep(1500 + i * 450).then(() => {
+            el.classList.add('word--is-animated')
+        })
+    }
+})();
